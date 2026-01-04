9ca0511075621476b6567ba57296947a21ec6cd2
timiajeojo <timiajeojo@gmail.com>
2026-01-04 04:19:48 -0500

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () =>{
+ return (
+  <footer className="py-8 px-6 border-t border-grey-800">
+   <div className="max-w-6xl mx-auto text-center text-grey-400">
+    <p>© 2026 Timi Ajeojo</p>
+   </div>
+  </footer>
+ )
+} 
+
+export default Footer
