3feac852e6152f8818466c5caa42b9ede6413284
timiajeojo <timiajeojo@gmail.com>
2026-01-04 18:57:18 -0500

    update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ export default function Hero() {
         <div className="hero-content">
           <h1 className="hero-title">Hi, I'm Timi</h1>
           <p className="hero-subtitle">I'm an enthusiastic full-stack developer with over 2 years of hands on experience in crafting engaging web experiences. Always eager to explore and apply new technologies, I'm ready to dive into whatever tools get the job done. I'm seeking an opportunity to contribute my creativity and growing skills to a collaborative team, helping to build impactful websites that make a difference</p>
+          <a href="mailto:timiajeojo@gmail.com" className="hero-email">timiajeojo@gmail.com</a>
         </div>
       </div>
     </section>
