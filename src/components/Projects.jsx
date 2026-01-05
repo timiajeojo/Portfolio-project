1978d61a6af0f2c79631b4df225f49b24dcdd653
timiajeojo <timiajeojo@gmail.com>
2026-01-05 01:59:43 -0500

    update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,28 +5,30 @@ export default function Projects() {
     {
       title: "Segment",
       description: "Segment is a platform I designed to connect people through books. The concept was born from recognizing the need for accessible literature in communities and creating a system where book owners could easily share their collections with eager readers.",
-      tags: ["Python", "OpenAI", "PostgreSQL", "Semantic Search", "Vector Database", "RAG", "Graph DB", "PyPI"],
-      website: "https://pypi.org/project/contextmemory/",
-      source: "https://github.com/samiksha0shukla/context-memory"
+      tags: ["React", "Tanstack Start", "superbase", "uploadthing"],
+      website: "https://segment-book.vercel.app/",
+      source: "https://github.com/timiajeojo/"
     },
     {
       title: "Wecare Health Management System",
       description: "Wecare is a healthcare management system built to simplify appointment scheduling and patient record tracking. It provides a user-friendly interface for both patients and healthcare providers, ensuring seamless communication and efficient care delivery.",
-      tags: ["Agentic AI", "MultiAgent Workflow", "Pydantic AI", "LangGraph", "Supabase", "Agentic RAG", "MCP", "Python"],
-      website: "https://aixai-mkfm.onrender.com/",
-      source: "https://github.com/samiksha0shukla/AIxAI"
+      tags: ["NextJS", "SQlite", "Drizzle ORM",],
+      website: "https://wecare-med.vercel.app/sign-in",
+      source: "https://github.com/timiajeojo/"
     },
     {
       title: "QR Tool",
       description: "A simple yet powerful tool for generating custom QR codes instantly. You can add links or text, choose colors, and download your QR in high quality. It’s fast, lightweight, and perfect for personal or business use. Built with a clean design optimized for mobile and desktop users.",
-      tags: ["Python", "Machine Learning", "Data Analytics", "Data Visualization", "Data Engineering", "Feature Engineering", "EDA", "Prediction Models"],
-      source: "https://github.com/samiksha0shukla/HomeSage"
+      tags: ["NextJS", "Tailwind CSS", "Radix UI", "Redux",],
+      website: "https://tool.qr.gauravsingh.co.in/",
+      source: "https://github.com/timiajeojo/"
     },
     {
       title: "Chat with PDF",
       description: "A web app that lets you upload any PDF and chat directly with its contents. It uses AI to read and understand the document, giving accurate, context-based answers. Perfect for summarizing long files or finding key details quickly.",
-      tags: ["React", "FastAPI", "Pydantic", "SQLAlchemy", "Python", "Workflow Automation", "Real-time"],
-      source: "https://github.com/samiksha0shukla/n8n"
+      tags: ["React", "NextJS", "Postgres", "Typescript",],
+      website: "https://chat-with-pdf-zeta-six.vercel.app/",
+      source: "https://github.com/timiajeojo/"
     },
     {
       title: "Transformers Research",
