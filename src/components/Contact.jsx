a2770043ee49d881497c7befb3d4bb2c9c7e5538
timiajeojo <Timi>
2026-01-05 03:19:01 -0500

    update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,18 +6,10 @@ export default function Contact() {
       <div className="section-container">
         <h2 className="section-title">Get in Touch</h2>
         <p className="contact-text">
-          Want to chat? Just shoot me a dm{' '}
-          <a href="https://x.com/Samiksha2908" className="about-link">with a direct question on twitter</a>
+          Want to chat? Just shoot me an email at{' '}
+          <a href="mailto:timiajeojo@gmail.com" className="about-link">timiajeojo@gmail.com</a>
           {' '}and I'll respond whenever I can. I will ignore all soliciting.
         </p>
-        <div className="contact-buttons">
-          <a href="https://www.linkedin.com/in/samiksha-shukla-7b2207217/" className="contact-button contact-button-primary">
-            LinkedIn
-          </a>
-          <a href="https://x.com/Samiksha2908" className="contact-button contact-button-secondary">
-            X
-          </a>
-        </div>
       </div>
     </section>
   );
